7d381b5a8f0d61333f4e9447a1f0382a31600068
Rostislav Raykov <me@zbrox.com>
2024-11-12 17:27:40 +0100

    feat: add checking of url, updating and creating a bookmark

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,8 @@ pub enum LinkDingError {
     SendHttpError(#[from] ureq::Error),
     #[error("Could not parse JSON response from API")]
     JsonParse(#[from] std::io::Error),
+    #[error("Could not serialize JSON body")]
+    JsonSerialize(#[from] serde_json::Error),
 }
 
 #[derive(Debug, Clone)]
@@ -24,6 +26,9 @@ pub enum Endpoint {
     ListBookmarks(ListArgs),
     ListArchivedBookmarks(ListArgs),
     GetBookmark(i32),
+    CheckUrl(String),
+    CreateBookmark,
+    UpdateBookmark(i32),
 }
 
 impl TryInto<http::Uri> for Endpoint {
@@ -31,21 +36,24 @@ impl TryInto<http::Uri> for Endpoint {
 
     fn try_into(self) -> Result<http::Uri, Self::Error> {
         match self.clone() {
-            Self::ListBookmarks(args) => {
+            Self::ListBookmarks(args) | Self::ListArchivedBookmarks(args) => {
                 let query_string = args.query_string();
                 let path: String = self.into();
                 http::Uri::try_from(format!("{}{}", path, query_string))
                     .map_err(LinkDingError::InvalidUrl)
             }
-            Self::ListArchivedBookmarks(args) => {
-                let query_string = args.query_string();
-                let path: String = self.into();
-                http::Uri::try_from(format!("{}{}", path, query_string))
-                    .map_err(LinkDingError::InvalidUrl)
-            }
-            Self::GetBookmark(id) => {
+            Self::GetBookmark(id) | Self::UpdateBookmark(id) => {
                 let path: String = self.into();
                 http::Uri::try_from(format!("{}{}", path, id)).map_err(LinkDingError::InvalidUrl)
+            }
+            Self::CheckUrl(url) => {
+                let path: String = self.into();
+                http::Uri::try_from(format!("{}?url={}", path, url))
+                    .map_err(LinkDingError::InvalidUrl)
+            }
+            Self::CreateBookmark => {
+                let path: String = self.into();
+                http::Uri::try_from(path).map_err(LinkDingError::InvalidUrl)
             }
         }
     }
@@ -56,7 +64,10 @@ impl QueryString for Endpoint {
         match self {
             Self::ListBookmarks(args) => args.query_string(),
             Self::ListArchivedBookmarks(args) => args.query_string(),
-            Self::GetBookmark(_) => "".to_string(),
+            Self::GetBookmark(_)
+            | Self::CheckUrl(_)
+            | Self::CreateBookmark
+            | Self::UpdateBookmark(_) => "".to_string(),
         }
     }
 }
@@ -66,7 +77,11 @@ impl Into<String> for Endpoint {
         match &self {
             Self::ListBookmarks(_) => "/api/bookmarks/".to_string(),
             Self::ListArchivedBookmarks(_) => "/api/bookmarks/archived/".to_string(),
-            Self::GetBookmark(id) => format!("/api/bookmarks/{}/", &id),
+            Self::GetBookmark(id) | Self::UpdateBookmark(id) => {
+                format!("/api/bookmarks/{}/", &id)
+            }
+            Self::CheckUrl(_) => "/api/bookmarks/check/".to_string(),
+            Self::CreateBookmark => "/api/bookmarks/".to_string(),
         }
     }
 }
@@ -77,6 +92,9 @@ impl Into<http::Method> for Endpoint {
             Self::ListBookmarks(_) => http::Method::GET,
             Self::ListArchivedBookmarks(_) => http::Method::GET,
             Self::GetBookmark(_) => http::Method::GET,
+            Self::CheckUrl(_) => http::Method::GET,
+            Self::CreateBookmark => http::Method::POST,
+            Self::UpdateBookmark(_) => http::Method::PATCH,
         }
     }
 }
@@ -109,6 +127,90 @@ pub struct ListBookmarksResponse {
     pub next: Option<String>,
     pub previous: Option<String>,
     pub results: Vec<Bookmark>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[cfg_attr(feature = "ffi", derive(uniffi::Record))]
+pub struct PageMetadata {
+    pub url: String,
+    pub title: Option<String>,
+    pub description: Option<String>,
+    pub preview_image: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[cfg_attr(feature = "ffi", derive(uniffi::Record))]
+pub struct CheckUrlResponse {
+    pub bookmark: Option<Bookmark>,
+    pub metadata: PageMetadata,
+    pub auto_tags: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[cfg_attr(feature = "ffi", derive(uniffi::Record))]
+pub struct CreateBookmarkBody {
+    pub url: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub title: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub notes: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub web_archive_snapshot_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub favicon_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub preview_image_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub is_archived: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub unread: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub shared: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub tag_names: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub date_added: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub date_modified: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub website_title: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub website_description: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[cfg_attr(feature = "ffi", derive(uniffi::Record))]
+pub struct UpdateBookmarkBody {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub title: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub notes: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub web_archive_snapshot_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub favicon_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub preview_image_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub is_archived: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub unread: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub shared: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub tag_names: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub date_added: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub date_modified: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub website_title: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub website_description: Option<String>,
 }
 
 #[derive(Debug, Default, Clone)]
@@ -189,6 +291,35 @@ impl LinkDingClient {
     pub fn get_bookmark(&self, id: i32) -> Result<Bookmark, LinkDingError> {
         let endpoint = Endpoint::GetBookmark(id);
         let request = self.prepare_request(endpoint)?.body(())?;
+        let body: Bookmark = ureq::run(request)?.body_mut().read_json()?;
+        Ok(body)
+    }
+
+    pub fn check_url(&self, url: &str) -> Result<CheckUrlResponse, LinkDingError> {
+        let endpoint = Endpoint::CheckUrl(url.to_string());
+        let request = self.prepare_request(endpoint)?.body(())?;
+        let body: CheckUrlResponse = ureq::run(request)?.body_mut().read_json()?;
+        Ok(body)
+    }
+
+    pub fn create_bookmark(&self, body: CreateBookmarkBody) -> Result<Bookmark, LinkDingError> {
+        let endpoint = Endpoint::CreateBookmark;
+        let request = self
+            .prepare_request(endpoint)?
+            .body(serde_json::to_string(&body)?)?;
+        let body: Bookmark = ureq::run(request)?.body_mut().read_json()?;
+        Ok(body)
+    }
+
+    pub fn update_bookmark(
+        &self,
+        id: i32,
+        body: UpdateBookmarkBody,
+    ) -> Result<Bookmark, LinkDingError> {
+        let endpoint = Endpoint::UpdateBookmark(id);
+        let request = self
+            .prepare_request(endpoint)?
+            .body(serde_json::to_string(&body)?)?;
         let body: Bookmark = ureq::run(request)?.body_mut().read_json()?;
         Ok(body)
     }
