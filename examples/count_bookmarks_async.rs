a6284d99bb775ed72cb00025e0b5d69a80ee9a66
Rostislav Raykov <me@zbrox.com>
2026-04-29 00:36:34 +0200

    docs: note async support and add async example

---
+++ examples/count_bookmarks_async.rs
@@ -0,0 +1,30 @@
+use linkding::{LinkDingAsyncClient, ListBookmarksArgs};
+
+#[tokio::main]
+async fn main() {
+    let linkding_host =
+        std::env::var("LINKDING_HOST").unwrap_or("http://localhost:9090".to_string());
+    let linkding_token =
+        std::env::var("LINKDING_TOKEN").expect("LINKDING_TOKEN env variable is not set");
+    let linkding_client = LinkDingAsyncClient::new(&linkding_host, &linkding_token);
+
+    let mut total_bookmarks = 0;
+    let mut offset = 0;
+
+    loop {
+        let response = linkding_client
+            .list_bookmarks(ListBookmarksArgs {
+                offset: Some(offset),
+                ..Default::default()
+            })
+            .await
+            .expect("Couldn't fetch bookmarks");
+        total_bookmarks += response.results.len();
+        if response.next.is_none() {
+            break;
+        }
+        offset += 100;
+    }
+
+    println!("Total bookmarks: {}", total_bookmarks);
+}
